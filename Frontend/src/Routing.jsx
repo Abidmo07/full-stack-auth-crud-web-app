f327a7a5b340da4243eb12d6df94305e0b95ee61
Abidmo07 <ma.abid@esi-sba.dz>
2024-09-16 14:44:31 +0100

    adding the userform in routing

--- Frontend/src/Routing.jsx
+++ Frontend/src/Routing.jsx
@@ -5,13 +5,16 @@ import Users from "./Pages/Users"
 import DefaultLayout from "./components/DefaultLayout"
 import GuestLayout from "./components/GuestLayout"
 import Dashboard from "./Pages/Dashboard"
+import UsersForm from "./Pages/UsersForm"
 
 export default function Routing() {
   return (
  <>
  <Routes>
     <Route path="/" element={<DefaultLayout/>}>
-    <Route path="/users" element={<Users/>} /> 
+    <Route path="/users" element={<Users/>} />
+    <Route path="/users/new" element={<UsersForm key="create" />} />
+    <Route path="/users/:id" element={<UsersForm key="update" />} />
     <Route path="/dashboard" element={<Dashboard/>} /> 
     </Route> 
 
